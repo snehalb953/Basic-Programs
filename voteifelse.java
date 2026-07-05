23455e49735902dfcc0955104bc8a4b19855c5f6
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:57:04 +0530

    Add files via upload

---
+++ voteifelse.java
@@ -0,0 +1,16 @@
+class voteifelse
+{
+  public static void main(String[] args)
+    {
+      int age=10;
+      
+      if(age>18)
+       {
+          System.out.println("Eligible");
+       }
+      else
+      {
+         System.out.println("Not Eligible");
+     }
+   }
+}
