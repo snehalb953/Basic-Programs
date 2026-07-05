e7e4655c73e7b2320be55e9a4208229f2779e72a
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:46:34 +0530

    Add files via upload

---
+++ Eoifelse.java
@@ -0,0 +1,18 @@
+class Eoifelse
+{
+   public static void main(String[] args)
+     {
+       System.out.println("start");
+       int a=20;
+       if(a%2==0)
+        {
+           System.out.println("even number");
+      
+        }
+      else
+       {
+         System.out.println("odd number");
+       }
+   System.out.println("stop");
+    }
+}
