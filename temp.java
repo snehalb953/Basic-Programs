0e67815db1473fccdfd3b43c69f639d7b8a60690
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:56:46 +0530

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,14 @@
+class temp
+{
+  public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int temp;
+           temp=a;
+           a=b;
+           b=temp;
+         System.out.println("before swapping"+a+" "+b);
+          System.out.println("After swapping"+a+" "+b);
+    }
+}
