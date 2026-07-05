f48b588ad0651c382d73f4da38817c4636a3db99
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:41:48 +0530

    Add files via upload

---
+++ bigwithoutl.java
@@ -0,0 +1,11 @@
+class bigwithoutl
+{
+  public static void main(String[] args)
+  {
+    int a=40;
+    int b=200;
+    int c=300;
+    int res=a>b? (a>c?a:c):(b>c?b:c);
+    System.out.println(res);
+  }
+}
