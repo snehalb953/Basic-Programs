9800c17a0e93979ad242a4ea72e29eebcc8e972f
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:44:22 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,24 @@
+class days
+{
+  public static void main(String[] args)
+   {
+      String day="mon";
+       switch(day)
+        {
+           case "mon":System.out.println("kannada");
+                       break;
+           case "tue":System.out.println("english");
+                       break;
+           case "wed":System.out.println("hindi");
+                       break;
+           case "thu":System.out.println("maths");
+                       break;
+           case "fri":System.out.println("science");
+                       break;
+           case "sat":System.out.println("social");
+                       break;
+              default:System.out.println("invalid");
+       }
+   }
+}
+                      
