3489cdd70d7641c96cfa81fd7b2e5af57e2f9562
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:45:06 +0530

    Add files via upload

---
+++ Distinct.java
@@ -0,0 +1,14 @@
+class Distinct
+{
+   public static void main(String[] args)
+     {
+       int x=20;
+       int y=30;
+       int z=20;
+         if(x!=y && x!=z && y!=z)
+             System.out.println("Distinct");
+         else
+             System.out.println("Not Distinct");
+    }
+}
+
