c4d1cb267ca5adf508cbf72b0b27825877b02e3c
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:50:11 +0530

    Add files via upload

---
+++ posnegifelse.java
@@ -0,0 +1,16 @@
+class posnegifelse
+{
+  public static void main(String[] args)
+    {
+      int a=20;
+      
+      if(a>=0)
+       {
+          System.out.println("positive");
+       }
+      else
+       {
+         System.out.println("Negative");
+       }
+    }
+}
