3125166d2c6a7f6dce40d512c89045377b84788c
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:54:04 +0530

    Add files via upload

---
+++ Small2no.java
@@ -0,0 +1,16 @@
+class Small2no
+{
+  public static void main(String[] args)
+   {
+     int a=45;
+     int b=30;
+     if(a<b)
+      {
+         System.out.println(" a is smaller than b ");
+      }
+     else 
+     {
+       System.out.println("b is smaller than a");
+     }
+   }
+}
