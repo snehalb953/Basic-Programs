63903a7631b873df75c49712c487b276c8fe9b8f
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:48:35 +0530

    Add files via upload

---
+++ Forno.java
@@ -0,0 +1,12 @@
+class Forno
+{
+   public static void main(String[] args)
+     {
+       int n=20;
+        for(int i=10;i<=n;i++)
+         {
+           System.out.println(i);
+         }
+     }
+}
+ 
