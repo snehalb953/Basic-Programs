7d276a8515c6fb916d563245ceff06eb8394cfe2
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:58:37 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,16 @@
+class variable
+{
+  public static void main(String[] args)
+   {
+     int a=10;
+     System.out.println(a);
+       {
+         String name="Sneha";
+         System.out.println(name);
+       }
+       {
+         int b=20;
+         System.out.println(b);
+        }
+   }
+}
