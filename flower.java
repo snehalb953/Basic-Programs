be6f95df142392cfa76b74052eac35e02cced8f1
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:48:16 +0530

    Add files via upload

---
+++ flower.java
@@ -0,0 +1,17 @@
+class flower
+{
+   public static void main(String[] args)
+     {
+       int n=25;
+       switch(n%2)
+         {
+           case 0:System.out.println("even");
+                  break;
+         default:System.out.println("odd");
+                 
+         }
+     }
+}
+
+            
+            
