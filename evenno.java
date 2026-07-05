81c15de196162805b7021c06f73ce9df830d6afe
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:47:17 +0530

    Add files via upload

---
+++ evenno.java
@@ -0,0 +1,12 @@
+class evenno
+{
+   public static void main(String[] asrgs)
+     {
+       int n=50;
+        for(int i=1;i<=n;i++)
+          {
+             if(i%2==0)
+             System.out.println(i);
+         }
+     }
+}
