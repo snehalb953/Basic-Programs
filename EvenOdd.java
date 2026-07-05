645dddb7983e7b446f41747469727667697b661a
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:47:43 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd
+{
+  public static void main(String[] args)
+   {
+    int n=25;
+    int m=40;
+    String res=n%2==0?"even":"odd";
+    String res1=n%2==1?"odd":"even";
+    System.out.println(res);
+    System.out.println(n+" is "+res+" number ");
+    System.out.println(n+" is "+res1+" number ");
+
+   }
+}
+
