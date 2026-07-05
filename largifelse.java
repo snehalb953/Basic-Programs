8853e03385026efdbcfbd03872625ebac2fce34f
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:49:28 +0530

    Add files via upload

---
+++ largifelse.java
@@ -0,0 +1,16 @@
+class largifelse
+{
+  public static void main(String[] args)
+    {
+      int a=50;
+      int b=40;
+      if(a>b)
+       {
+          System.out.println("a is larger");
+       }
+      else
+      {
+         System.out.println("b is larger");
+     }
+   }
+}
