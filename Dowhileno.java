81388107236e39b2d3bc9f7b623af0f2366266a2
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:45:49 +0530

    Add files via upload

---
+++ Dowhileno.java
@@ -0,0 +1,13 @@
+class Dowhileno
+{
+   public static void main(String[] args)
+     {
+       int n=20;
+       int i=10;
+        do
+         {
+           System.out.println(i);
+                i++;
+         }while(i<=n);
+     }
+}
