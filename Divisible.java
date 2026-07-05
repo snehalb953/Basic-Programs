284af7e0f9d98cbb37b11f70b63bf48a12c1588d
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:45:26 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,9 @@
+class Divisible
+{
+  public static void main(String[] args)
+   {
+     int n=76;
+     String res=n%5==0?"divisible":"Not";
+     System.out.println(n+" is "+res+" by "+5);
+   }
+}
